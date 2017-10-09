7f8e6c2268787c2461232328a339024faf288395
Kirka <kirka121@gmail.com>
2017-10-09 18:26:30 -0400

    0.9

--- lib/card_extractor.rb
+++ lib/card_extractor.rb
@@ -88,7 +88,7 @@ module MTGExtractor
     def regex_name(n=extract_name)
       Regexp.quote(n).
         sub("rathi\\ Berserker","(?:AE|Æ|)rathi Berserker").
-        sub("Yet\\ Another\\ Aether\\ Vortex","Yet Another Æther Vortex").
+        sub("Yet\\ Another\\ Æther\\ Vortex","Yet Another Aether Vortex").
         sub("Æ", "Ae").
         gsub(/ +/,"\s*").
         sub(/^(X+)/,"(?:\\1)?")
